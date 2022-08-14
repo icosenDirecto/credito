865f179d08f35d9624b26499e5575ccc972b7c7b
icosenDirecto <icosentino@directo.com.ar>
2022-08-13 23:00:44 -0300

    v1.2.4

--- functions/fetchNotion.js
+++ functions/fetchNotion.js
@@ -1,19 +1,7 @@
-const { Client } = require("@notionhq/client")
-
-const { NOTION_KEY, NOTION_DB } = process.env;
-
-// Initializing a client
-const notion = new Client({
-  auth: NOTION_KEY,
-})
-
 exports.handler = async function (event, context) {
-    const response = await notion.databases.query({
-        database_id: NOTION_DB,
-    });
     return {
         statusCode: 200,
-        body: JSON.stringify(response
-        ),
+        body: JSON.stringify ({
+        message: 'Hello World' }),
     };
 };
