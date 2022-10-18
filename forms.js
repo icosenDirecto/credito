aa7492be03ef09bfbccaceba6c31d2c6f83d875f
icosenDirecto <111062633+icosenDirecto@users.noreply.github.com>
2022-10-18 08:17:10 -0300

    v2.0

--- forms.js
+++ forms.js
@@ -4,7 +4,7 @@ const result = document.getElementById('result');
 form.addEventListener('submit', function(e) {
     const formData = new FormData(form);
     const Celular = formData.get('cellphone');
-    const mySubject = '${Celular}';
+    const mySubject = `${Celular}`;
     formData.append("subject", mySubject);
 
     e.preventDefault();
